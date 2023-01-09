4c4b080e55b179c634321bf873451c0ee6e7b3de
christina <christinayxh@163.com>
2023-01-09 20:48:30 +0800

    修复icon nav 点击区域过小问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -11,17 +11,18 @@ const NavWrapper = styled.nav`
     > li{
       width: 33.3333%;
       text-align:center;//文本内容居中
-      display: flex;//分别居中
-      flex-direction: column;//icon和文字 纵向分布；
-      padding: 4px 0;
-      justify-content: center;//X轴中间--start - space-between/space-around /space-evenly 
-      //align-content Y轴--start/ space-between/space-around /space-evenly
-       
-      align-items: center;//Y轴 item（元素）stretch-center-start-end 自身大小居中 
-      // justify-item stretch-center-start-end 自身大小居中 
-      .icon {
-        width: 24px;      
-        height: 24px;
+      a{
+        display: flex;//分别居中
+        flex-direction: column;//icon和文字 纵向分布；
+        padding: 4px 0;
+        justify-content: center;//X轴中间--start - space-between/space-around /space-evenly 
+        //align-content Y轴--start/ space-between/space-around /space-evenly
+        align-items: center;//Y轴 item（元素）stretch-center-start-end 自身大小居中 
+        // justify-item stretch-center-start-end 自身大小居中 
+        .icon {
+          width: 24px;
+          height: 24px;
+        }
       }
     }
   }
@@ -32,16 +33,25 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="tags"/>
-          <Link to="/tags">标签页</Link>
+
+          <Link to="/tags">
+            <Icon name="tags"/>
+            标签
+          </Link>
         </li>
         <li>
-          <Icon name="dollar"/>
-          <Link to="/money">记账页</Link>
+
+          <Link to="/money">
+            <Icon name="dollar"/>
+            记账页
+          </Link>
         </li>
         <li>
-          <Icon name="charts"/>
-          <Link to="/statistics">统计页</Link>
+
+          <Link to="/statistics">
+            <Icon name="charts"/>
+            统计页
+          </Link>
         </li>
       </ul>
     </NavWrapper>
