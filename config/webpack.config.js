bf888184051504f70da38c1206b66cfeb8db4577
christina <christinayxh@163.com>
2023-01-09 22:40:37 +0800

    money.tsx ui 完成

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -353,9 +353,9 @@ module.exports = function (webpackEnv) {
               use: [
                 {loader: 'svg-sprite-loader', options: {}},
                 {loader: 'svgo-loader', options: {
-                  plugins:[
-                    {removeAttrs:{attrs:'fill'}}
-                  ]
+                  // plugins:[
+                  //   {removeAttrs:{attrs:'fill'}}
+                  // ]
                   }
                 }
               ]
