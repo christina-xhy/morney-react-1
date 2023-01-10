e1c0c6520b008afd2a67be5f3ba388a2b4742318
christina <christinayxh@163.com>
2023-01-10 14:32:16 +0800

    money.categorySection样式设置

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -49,7 +49,29 @@ const NotesSection =styled.section`
   }
 `
 
-const CategorySection=styled.section``
+const CategorySection=styled.section`
+ ul{
+   display:flex;
+   background:lightsteelblue;
+   font-size: 24px;
+  >li{
+    width:50%;
+    text-align:center;
+    padding:16px 0 ;
+    position:relative;//夫相子绝；
+    &.selected::after{
+      content:'';
+      display:block;//变为block元素
+      height:3px;//不需要border，盒子变为实心的线条；
+      background:indigo;
+      position:absolute;//设置了绝对定位后，元素会消失，需要加width：100%
+      bottom:0;//确定绝对定位的位置
+      width:100%;//显示出元素；
+      left:0;//防止bug出现
+    }
+  }
+}
+`
 
 const NumberPad = styled.section``
 
@@ -72,8 +94,10 @@ function Money() {
         </label>
       </NotesSection>
       <CategorySection>
-        <li>支出</li>
-        <li>收入</li>
+         <ul >
+           <li className='selected'>支出</li>
+           <li>收入</li>
+         </ul>
       </CategorySection>
       <NumberPad>
          <div>100</div>
