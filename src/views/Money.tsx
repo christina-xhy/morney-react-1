587838a0209e7621e075e4c36bfd355a69987850
christina <christinayxh@163.com>
2023-01-10 14:10:17 +0800

    money.NotesSection样式设置

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -27,11 +27,27 @@ const TagsSection =styled.section`
   }
   
 `;
-const TagList =styled.ol`
- 
-`
 
-const NotesSection =styled.section``
+const NotesSection =styled.section`
+  background-color:#f5f5f5;
+  padding:10px 16px;
+  font-size:14px;
+  > label{
+    display:flex; //flex布局
+    align-items:center;
+    > span{
+      margin-right:16px;//元素之间的距离
+      white-space: nowrap;
+    }
+    >input{
+      background: none;
+      border:none;
+      height:72px;
+      width:100%;//输入框100%，会影响另一个元素（span）的距离，设置span的white-space ：nowrap;
+      display:block;//盒子的宽度占满当前一行会出现另起一行，则在label 父元素设置display：flex;
+    }
+  }
+`
 
 const CategorySection=styled.section``
 
@@ -52,7 +68,7 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type='text'/>
+          <input type='text' placeholder='在这里添加备注'/>
         </label>
       </NotesSection>
       <CategorySection>
