57c6e3780edeec8d5664c0cee41deb436916fca9
christina <christinayxh@163.com>
2023-01-11 00:41:15 +0800

    删除+-*/

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -15,7 +15,7 @@ const Wrapper= styled.section`
   },
   > .pad{
   >button{
-    width:20%;//确定一排5个元素；
+    width:25%;//确定一排5个元素；
     height:64px;//设置元素高度；
     float:left;//错位交叉；父左
     border:none;
@@ -24,44 +24,40 @@ const Wrapper= styled.section`
       float:right;//浮动定位子右；如果爷爷display：flex则孙子浮动了，爸爸不会坍塌。
       //另一个方法是清楚浮动定位clearfix，现在scss设置初始化，然后直接在爸爸后面添加伪元素即可
     }
-    &:nth-child(2),
-    &:nth-child(6){
+    &:nth-child(1) {
       background: #E0E0E0;
     }
-    &:nth-child(3),
-    &:nth-child(7),
-    &:nth-child(11)
+    &:nth-child(2),
+    &:nth-child(5)
     {
       background:#D3D3D3;
     }
-    &:nth-child(4),
-    &:nth-child(8),
-    &:nth-child(12),
-    &:nth-child(16){
+    &:nth-child(3),
+    &:nth-child(6),
+    &:nth-child(9){
       background:#C1C1C1;
     }
-    &:nth-child(5),
-    &:nth-child(9),
+    &:nth-child(4),
+    &:nth-child(7),
     &:nth-child(13),
-    &:nth-child(17)
+    &:nth-child(10)
     {
       background:#B8B8B8 ;
     } 
-      &:nth-child(10),
+      &:nth-child(11),
       &:nth-child(14),
-      &:nth-child(18)
+      &:nth-child(8)
       {
         background:#A9A9A9;
       }
-   
-    &:nth-child(19)
+    &:nth-child(15)
     {
       background:#B8B8B8;
     }
-    &:nth-child(20),
-    &:nth-child(15)
-    {
-      background:#9A9A9A;
+    &:nth-child(12)
+    &:nth-child(16)
+    { 
+      background:#A9A9A9;
   }
 }
 `
@@ -114,13 +110,13 @@ const NumberPad :React.FC =()=>{
 
         //
         if(output.length === 1){
-          setOutput('0')
+          setOutput('')
         }else{
           setOutput(output.slice(0,-1))
         }
         break;
       case '清空':
-        setOutput('0')
+        setOutput('')
         break;
       case 'Ok':
         break;
@@ -128,29 +124,25 @@ const NumberPad :React.FC =()=>{
   }
   return(
     <Wrapper>
-        <div className='output'>
+        <div className='output' >
           {output}
         </div>
         <div className='pad clearfix' onClick ={onClickButtonWrapper}>
           <button>1</button>
           <button>2</button>
           <button>3</button>
-          <button>+</button>
           <button>删除</button>
           <button>4</button>
           <button>5</button>
           <button>6</button>
-          <button>-</button>
           <button>清空</button>
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>*</button>
           <button className='ok'>Ok</button>
           <button>0</button>
           <button>00</button>
           <button>.</button>
-          <button>/</button>
         </div>
     </Wrapper>
   )
