7a8dbb12901ba25c259ed12eaf4f0feea9bc441a
christina <christinayxh@163.com>
2023-01-11 00:04:59 +0800

    bugfix：长度显示过长 ，解决按钮功能实现

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -67,37 +67,64 @@ const Wrapper= styled.section`
 `
 
 const NumberPad :React.FC =()=>{
-  const [output,setOutput] = useState<string>('0')
+  const [output,_setOutput] = useState<string>('0')
+  const setOutput = (output:string)=>{
+    if(output.length>16){
+    output = output.slice(0,16)
+    }else if(output.length === 0){
+      output = '0'
+    }
+    _setOutput(output)
+  }
   const onClickButtonWrapper = (e:React.MouseEvent)=>{
     const text = (e.target as HTMLButtonElement) .textContent
     if(text === null){return}
     switch(text){
-      case'0':
-      case'00':
-      case'1':
-      case'2':
-      case'3':
-      case'4':
-      case'5':
-      case'6':
-      case'7':
-      case'8':
-      case'9':
-      case'.':
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
         if(output === '0'){
           setOutput(text)
         }else{
           setOutput(output+text)
         }
         break;
-      case'删除':
+      case '00':
+        if(text === '00' && output === '0'){
+          return
+        }else{
+          setOutput(output+text)
+        }
         break;
-      case'清空':
+      case '.':
+        if(output.indexOf('.') >= 0){
+          return
+        }else{
+          setOutput(output+'.')
+        }
+        break;
+      case '删除':
+
+        //
+        if(output.length === 1){
+          setOutput('0')
+        }else{
+          setOutput(output.slice(0,-1))
+        }
+        break;
+      case '清空':
+        setOutput('0')
         break;
       case 'Ok':
         break;
     }
-
   }
   return(
     <Wrapper>
