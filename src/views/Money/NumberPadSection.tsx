7462cb4f38ae58b27dff09a315f47b04d9b6f416
christina <christinayxh@163.com>
2023-01-10 23:14:18 +0800

    NumberPad功能 wip

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import React from 'react';
+import React, {ReactEventHandler, useState} from 'react';
 
 const Wrapper= styled.section`
   display:flex;
@@ -67,10 +67,44 @@ const Wrapper= styled.section`
 `
 
 const NumberPad :React.FC =()=>{
+  const [output,setOutput] = useState<string>('0')
+  const onClickButtonWrapper = (e:React.MouseEvent)=>{
+    const text = (e.target as HTMLButtonElement) .textContent
+    if(text === null){return}
+    switch(text){
+      case'0':
+      case'00':
+      case'1':
+      case'2':
+      case'3':
+      case'4':
+      case'5':
+      case'6':
+      case'7':
+      case'8':
+      case'9':
+      case'.':
+        if(output === '0'){
+          setOutput(text)
+        }else{
+          setOutput(output+text)
+        }
+        break;
+      case'删除':
+        break;
+      case'清空':
+        break;
+      case 'Ok':
+        break;
+    }
+
+  }
   return(
     <Wrapper>
-        <div className='output'>100</div>
-        <div className='pad clearfix'>
+        <div className='output'>
+          {output}
+        </div>
+        <div className='pad clearfix' onClick ={onClickButtonWrapper}>
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -87,8 +121,8 @@ const NumberPad :React.FC =()=>{
           <button>*</button>
           <button className='ok'>Ok</button>
           <button>0</button>
+          <button>00</button>
           <button>.</button>
-          <button>%</button>
           <button>/</button>
         </div>
     </Wrapper>
