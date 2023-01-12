4c881cbcf08a62274b3404f6fd47021700a5ad66
christina <christinayxh@163.com>
2023-01-12 17:49:49 +0800

    Tag页面路由跳转 封装findTag元素//filter.(tag => tag.id === id[0])需要转为字符串类型

--- src/useTag.tsx
+++ src/useTag.tsx
@@ -11,7 +11,8 @@ const defaultTags =[
 //定义在useTags函数外面，数据则只渲染一次。或者用use其他的hook 包裹
 const useTags = ()=>{
   const [tags,setTags] = useState<{id: number,name: string}[]>(defaultTags)
-  return {tags,setTags} //必须 对象形式  return出去
+  const findTag = (id:number) => tags.filter(tag => tag.id === id)[0];//数组的第0项是vale=string
+  return {tags,setTags,findTag} //必须 对象形式  return出去
 }
 
 export default  useTags
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
+import useTags from '../useTag';
 
-const Tag: React.FC =(tag) =>{
+const Tag: React.FC =(props) =>{
+
+  type Params = {
+    id:string
+  }
+  const {findTag} = useTags()
+  let { id } = useParams<Params>()
+  const tag = findTag(parseInt(id));
   return(
-    <div>hello</div>
+    <div>{tag.name}</div>
   )
 }
-
-
 export {Tag}
 
