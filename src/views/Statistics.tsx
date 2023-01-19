984a9e62b7206060464d4380683484826c6f2170
christina <christinayxh@163.com>
2023-01-19 20:16:05 +0800

    statistics 重新构造数据

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 import day from 'dayjs'
 import {ReactNode} from 'react';
 import {Echarts} from '../components/echarts';
-import {getSymbolSize} from 'echarts/types/src/chart/graph/graphHelper';
+import _ from 'lodash';
 // ccc
 const ChartWrapper = styled.div`
     overflow: auto;
@@ -50,9 +50,16 @@ function Statistics() {
   const {getName} = useTags()
   //getName是useTags组件的数据，类型为 tags: {id: number, name: string}类型
   const selectRecords = records.filter(r => r.category === category)
+  const newList = records.filter(r => r.createdAt === createdAt )
+
+
   const hash : { [k:string] :RecordItem[] }= {} // {2023-1-14:[item,item],2023-1-14:[item,item],2023-1-14:[item,item]}
   //给这个盒子定义了一个key为标准寻找数据的对象，把数组创建为hash 对象  //
   // 如果没有在里面，那这个元素为【】，最后需要创建每一项的value传入这个对象
+  //forEach 没有返回值
+  //把相同日期的记录放在同一个hash对象里面，有相同key的不同对象做赛选
+  //key不相同则不传数据进去，
+  //key相同，则把value传进去
   selectRecords.forEach(r  => {
     const key = day(r.createdAt).format("YYYY-MM-DD")
       if(!(key in hash)){
@@ -61,9 +68,9 @@ function Statistics() {
         hash[key].push(r)
   })
   //先声明一个hash={} ,遍历selectRecords数组， 找到key都相同且对应的对象，传入hash 对象。。。
-
   //Obj.entries 把对象转为我们需要的数组类型----某些浏览器需要引入这个库(core-js)才可以使用entries
-  //['2023-01-14',{tagIds: Array(1), note: '', category: '+', amount: 300, createdAt: '2023-01-13T14:07:23.607Z'}]
+  //['2023-01-14',{tagIds: Array(1), note: '', category: '+', amount: 300, createdAt: '2023-01-13T14:07:23.607Z'}]，
+  // ['2023-01-14',{tagIds: Array(1), note: '', category: '+', amount: 300, createdAt: '2023-01-13T14:07:23.607Z'}]
   console.log(Object.entries(hash))
   //判断key的大小 用字符串比较法来排列时间顺序
   const array = Object.entries(hash).sort((a,b)=>{
@@ -72,6 +79,7 @@ function Statistics() {
          if (a[0] < b[0]) return 1 //1 旧日期
          return 0
   })
+
   const option={
   tooltip: {
     triggerOn:'click',
@@ -92,7 +100,7 @@ function Statistics() {
   xAxis: {
     type: 'category',
     data: [
-      '1', '2', '3', '4', '5', '6', '7','8','9','10',
+      '1', '2', '3', '4', '5', '6', '7','8', '9', '10',
       '11', '12', '13', '14', '15', '16', '17','18','19','20',
       '21', '22', '23', '24', '25', '26', '27','28','29','30',
     ],
@@ -121,6 +129,16 @@ function Statistics() {
     },
   ],
 }
+  const keyValueList =() =>{
+    const today = new Date();
+    const array =[];
+    for(let i=0;i <= 29;i++){
+      const dateString = day(today).subtract(i,'day').format("YYYY-MM-DD");
+      const found = _.find(newList,{createdAt})
+      array.push({date:dateString,value:found ? found.amount : 0 })
+    }
+  }
+//设置当前这个元素滚动的距离=滑动距离，则始终展示最右边的数据。
 const ref = useRef<HTMLDivElement>(null)
   useEffect(()=>{
     ref.current!.scrollLeft = ref.current!.scrollWidth
@@ -140,6 +158,8 @@ const ref = useRef<HTMLDivElement>(null)
         <CategorySection value ={category}
                          onChange ={(value)=> setCategory(value)}/>
       </CategoryWrapper>
+      {/*展示统计图形 Echarts*/}
+
       <ChartWrapper ref={ref}>
           <div className='chart-bar'>
             <Echarts option={option}/>
@@ -155,11 +175,12 @@ const ref = useRef<HTMLDivElement>(null)
                return(
                  <ItemWrapper >
                    <div className='tags oneLine'>
+                     {/*实现多个标签之间添加一个，*/}
                      {r.tagIds.map(tagId => <span key={tagId}>{getName(tagId)}</span>)
                          .reduce((result,span,index,array) =>
                          result.concat(index < array.length -1 ? [span,','] : [span]) ,[] as ReactNode[])
                      }
-                     {/*.reduce 为了添加，，且末尾也不添加逗号 */}
+                     {/*.reduce 为了添加，且末尾也不添加逗号 */}
                    </div>
                    {r.note && <div className='note'>
                      {r.note}
